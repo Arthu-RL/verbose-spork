68c3cc59487b9e8ac122ceb41518cd859128d463
Arthu-RL <arthurrlima29@gmail.com>
2024-06-15 02:12:40 -0300

    adding some initial raylib code study

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <thread>
 #include <raylib.h>
 
 #include "MyString/MyString.h"
 #include "Print.h"
 
-static Print *p;
+static Print *p;    
 
 static int width = 1280, height = 720;
 
@@ -15,18 +19,22 @@ int main() {
         p->error("Window Chashed!");
         return 1;
     }
+
+    SetWindowTitle("TaskerApp");
    
     while (!WindowShouldClose()) {
-        ClearBackground(DARKGRAY);
-        
         BeginDrawing();
+            ClearBackground(DARKGRAY);
+            DrawText("Congrats! You created your first window!", width*0.5 - MeasureText("Congrats! You created your first window!", 20)/2, height*0.5, 20, WHITE);
+             DrawText(("FPS: " + std::to_string(GetFPS())).c_str(), width*0.01, height*0.01, 20, WHITE);
         EndDrawing();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
     CloseWindow();
     
-
-    p->debug("Successfuly closed!\n");
+    p->debug("Successfuly closed!");
     p->print_in_color("bye", consoleColors.green);
 
     delete p;
